f8104f4e203972dbdd126b197c0b19d5ce3b356f
opilane <opilane@HP-CZC0359NNW>
2023-02-21 09:30:15 +0200

    Add project files.

---
+++ hmm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<int, string> koolid = new Dictionary<int, string>();
+  
+        koolid.Add(1, "Tartu Raatuse kool");
+        koolid.Add(2, "Tallinna Laagna Gümnaasium");
+        koolid.Add(3, "Tallinna TööstusHaridusKeskus");
+  
+        foreach (string kool in koolid.Values)
+        {
+            Console.WriteLine(kool);
+        }
+    }
+}
